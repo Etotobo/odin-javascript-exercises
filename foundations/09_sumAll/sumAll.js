253d4455c034549d73fa96146a4359cf4085acbf
etotobo <etotobo@gmail.com>
2026-06-26 19:14:21 +0100

    Edit the function sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a === b){
+        return a ;
+    }else if(
+        a < 0 || 
+        b < 0 || 
+        typeof a != 'number' || 
+        typeof b != 'number' || 
+        Number(a) % 1 != 0 || 
+        Number(b) % 1 != 0
+    ){
+        return "ERROR";
+    }else{
+        let smallerNum = a < b ? a : b;
+        let biggerNum =  a > b ? a : b;
+        let arr = [smallerNum];
+        for(let i = smallerNum; i <= biggerNum - 1; i++){
+            arr.push(++smallerNum);
+            console.log(arr);
+        }
+        return arr.reduce((acc, num) => acc + num, 0);
+    }       
 
 };
 
