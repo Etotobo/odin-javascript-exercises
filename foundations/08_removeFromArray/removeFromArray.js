2438bc4dd1774f5a6d4038f2b31d84e7442d876e
etotobo <etotobo@gmail.com>
2026-06-26 17:50:25 +0100

    Edit the function removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    return arr.filter(item => {
+        for(num of nums){
+            if(num === item) {
+                return false 
+            }else{
+                continue;
+            } 
+        }
+        return true;
+    })
 };
 
 // Do not edit below this line
