4dc4d13eb3c4590d5f09b1603124ebc86080e85f
etotobo <etotobo@gmail.com>
2026-06-26 17:02:32 +0100

    Edit the function repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if(num < 0){
+        newString = "ERROR";
+    }else{
+        let i = 1;
+        while(i <= num){
+            newString += string;
+            i++;
+        }
+    }
+    return newString;
 };
 
 // Do not edit below this line
