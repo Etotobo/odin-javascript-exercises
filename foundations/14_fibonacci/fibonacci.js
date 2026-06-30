9dd19c1c2cf347e4583842a62659f4636fd03ff6
etotobo <etotobo@gmail.com>
2026-06-30 09:01:04 +0100

    Create a function that returns a specific member of the Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(Number(num) < 0){
+        return "OOPS";
+    } else {
+        num = Number(num);
+        let fiboArr = [0, 1];
+        for(let i = 1; i < num; i++){
+            fiboArr.push(fiboArr[i-1]+fiboArr[i]);
+        }
+        return fiboArr[num];
+    }
 };
 
 // Do not edit below this line
