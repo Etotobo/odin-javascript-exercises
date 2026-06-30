64b28f347360133df7955ccefea149bfde8f68e5
etotobo <etotobo@gmail.com>
2026-06-30 08:36:10 +0100

    Update the palindrome checker function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+ const palindromes = function (str) {
+    const strArr = str.toLowerCase().split("").filter(item => /[a-zA-Z0-9]/.test(item));
+    const firstPart = strArr.slice(0,(strArr.length)/2 + 1).join("");
+    const lastPart = strArr.slice((strArr.length)/2).reverse().join("");
+    return firstPart === lastPart;
 };
 
 // Do not edit below this line
