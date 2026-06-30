744f3f7dcc2828b9c73c530b5cdb41aba194a2cc
etotobo <etotobo@gmail.com>
2026-06-30 09:43:04 +0100

    Add a function to get the oldest person of an array

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort((a, b) => {
+        a.yearOfDeath = typeof(a.yearOfDeath) === "number" ? a.yearOfDeath : new Date().getFullYear();
+        b.yearOfDeath = typeof(b.yearOfDeath) === "number" ? b.yearOfDeath : new Date().getFullYear();
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
