3824f28ae2d5f2994156a8a83cd988d80622614c
etotobo <etotobo@gmail.com>
2026-06-30 07:11:16 +0100

    Create a calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let result = base * base;
+	for(let i = 2; i < exp; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for(let i = 0; i < num; i++){
+    result = result * (num -i);
+  }
+  return result;
+
 };
 
 // Do not edit below this line
